f148445d2142e21900cc7b57bc5834816ca826fe
Maybelline Grace <69809000+MayTheCutie@users.noreply.github.com>
2025-04-25 18:58:00 -0400

    slowly updated cutoffs and other things. keep going <3

--- AccelerometerAnalyzer.py
+++ AccelerometerAnalyzer.py
@@ -7,43 +7,6 @@ import matplotlib.pyplot as mpl
 data_list = []
 properties_list = []
 data_names = []
-option = 0
-
-# This initial section will
-print("Input Data: (Absolute Path)")
-initial_data, output_dir_data, output_filename_data = io.csv_to_list()
-data_list.append(initial_data)
-data_names.append(output_filename_data.split("_.")[0].replace("_", " "))
-print("Plot Properties: (Absolute Path)")
-properties, output_dir_properties, output_properties = io.csv_to_list()
-properties_list.append(properties)
-while True:
-    print("Input Data: (Absolute Path)")
-    added_data, o_d_d_temp, o_f_d_temp = io.csv_to_list()
-    data_list.append(added_data)
-    data_names.append(o_f_d_temp.split("_.")[0].replace("_", " "))
-    print("Plot Properties: (Absolute Path)")
-    properties, o_d_p_temp, o_f_p_temp = io.csv_to_list()
-    properties_list.append(properties)
-    try:
-        option = int(input("""Do you want to input more data?
-                             (0) Yes
-                             (1) No\n"""))
-        if option == 1:
-            break
-    except ValueError:
-        print("Invalid input, stopping input loop.")
-        break
-
-plot_number = 2
-if properties_list[0][3] is not None and int(properties_list[0][3]) >= 2:
-    kernel_size = int(properties_list[0][3])
-else: kernel_size = 3
-max_freq = 500
-
-# Make the final plot be in Graphs dir
-output_filename = output_filename_data.split(".")
-filename = os.path.join(output_dir_data, "Graphs", f"All_{output_filename[0]}.png")
 
 def extract_plot_properties(props, index):
     """
@@ -102,16 +65,6 @@ def extract_plot_properties(props, index):
 
     return plot_type, upper_cutoff, lower_cutoff, window_size, max_freq, start_cutoff, end_cutoff
 
-
-# Use plotting to analyze
-i = 0
-filtered_data = []
-for idx, (plot_properties, data) in enumerate(zip(properties_list, data_list)):
-    print(f"Processing dataset {idx+1}...")
-    plot_type, upper_cutoff, lower_cutoff, window_size, max_freq, starter_time_cutoff, end_time_cutoff = extract_plot_properties(plot_properties, idx)
-
-    filtered_data.append(plt.data_things(data, upper_cutoff, lower_cutoff, starter_time_cutoff, end_time_cutoff))
-
 def get_cmap(n, name='hsv'):
     """Returns a function that maps each index in 0, 1, ..., n-1 to a distinct RGB color."""
     return mpl.colormaps[name]
@@ -144,4 +97,54 @@ def overlay_moving_avg_plots(data_lists, window, fn):
     ax.legend()
     fig.savefig(plt.filename_input(fn, "stacked-moving-avg"))
 
-overlay_moving_avg_plots(filtered_data, kernel_size, filename)
+def main():
+
+    # This initial section will force you to input two sets of data to begin with (but only use the
+    # initial data/plot props)
+    print("Input Data: (Absolute Path)")
+    initial_data, output_dir_data, output_filename_data = io.csv_to_list()
+    data_list.append(initial_data)
+    data_names.append(output_filename_data.split("_.")[0].replace("_", " "))
+    print("Plot Properties: (Absolute Path)")
+    properties, output_dir_properties, output_properties = io.csv_to_list()
+    properties_list.append(properties)
+    while True:
+        print("Input Data: (Absolute Path)")
+        added_data, o_d_d_temp, o_f_d_temp = io.csv_to_list()
+        data_list.append(added_data)
+        data_names.append(o_f_d_temp.split("_.")[0].replace("_", " "))
+        print("Plot Properties: (Absolute Path)")
+        properties, o_d_p_temp, o_f_p_temp = io.csv_to_list()
+        properties_list.append(properties)
+        try:
+            option = int(input("""Do you want to input more data?
+                                 (0) Yes
+                                 (1) No\n"""))
+            if option == 1:
+                break
+        except ValueError:
+            print("Invalid input, stopping input loop.")
+            break
+
+    if properties_list[0][3] is not None and int(properties_list[0][3]) >= 2:
+        kernel_size = int(properties_list[0][3])
+    else:
+        kernel_size = 3
+
+    # Make the final plot be in Graphs dir
+    output_filename = output_filename_data.split(".")
+    filename = os.path.join(output_dir_data, "Graphs", f"All_{output_filename[0]}.png")
+
+    # Use plotting to analyze
+    filtered_data = []
+    for idx, (plot_properties, data) in enumerate(zip(properties_list, data_list)):
+        print(f"Processing dataset {idx + 1}...")
+        plot_type, upper_cutoff, lower_cutoff, window_size, max_freq, starter_time_cutoff, end_time_cutoff = extract_plot_properties(
+            plot_properties, idx)
+
+        filtered_data.append(plt.data_things(data, upper_cutoff, lower_cutoff, starter_time_cutoff, end_time_cutoff))
+
+    overlay_moving_avg_plots(filtered_data, kernel_size, filename)
+
+if __name__ == "__main__":
+    main()
